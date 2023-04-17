33bcb38e1dec7db6e3fb420fcc0bd0164c50284b
Tomás Pinho <me@tomaspinho.com>
2023-04-17 12:05:59 +0100

    bump version to match pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-salesforce',
-      version='1.5.0+meltano',
+      version='1.5.5+meltano',
       description='Singer.io tap for extracting data from the Salesforce API',
       author='Stitch',
       url='https://singer.io',
